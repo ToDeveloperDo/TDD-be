2a27ca988a9472adb43e5b41208f6a9e14ec0af3
JunRock <wnstjr120422@naver.com>
2024-07-07 23:08:14 +0900

    refactor : swagger설정 수정

--- src/main/kotlin/io/junseok/todeveloperdo/swagger/SwaggerConfig.kt
+++ src/main/kotlin/io/junseok/todeveloperdo/swagger/SwaggerConfig.kt
@@ -1,14 +1,8 @@
-package io.junseok.mealmate.swagger
+package io.junseok.todeveloperdo.swagger
 
+import org.springdoc.core.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import springfox.documentation.builders.ApiInfoBuilder
-import springfox.documentation.builders.PathSelectors
-import springfox.documentation.builders.RequestHandlerSelectors
-import springfox.documentation.service.ApiInfo
-import springfox.documentation.spi.DocumentationType
-import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration //스프링 실행시 설정파일 읽어드리기 위한 어노테이
 class SwaggerConfig {
