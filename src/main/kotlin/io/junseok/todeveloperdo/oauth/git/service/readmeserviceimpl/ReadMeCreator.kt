caa1aa3b00481a31b39a1d3eefa70929e019f471
JunRock <wnstjr120422@naver.com>
2024-07-10 23:53:06 +0900

    feat : readme 구현체 분리

---
+++ src/main/kotlin/io/junseok/todeveloperdo/oauth/git/service/readmeserviceimpl/ReadMeCreator.kt
@@ -0,0 +1,43 @@
+package io.junseok.todeveloperdo.oauth.git.service.readmeserviceimpl
+
+import io.junseok.todeveloperdo.oauth.git.client.GtiHubReadMeClient
+import io.junseok.todeveloperdo.oauth.git.dto.request.fileCommitRequestInit
+import io.junseok.todeveloperdo.oauth.git.service.GitHubService
+import io.junseok.todeveloperdo.oauth.git.util.toStringDateTime
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+
+@Component
+class ReadMeCreator(
+    private val gitHubReadMeClient: GtiHubReadMeClient
+) {
+    fun readMeContentCreate(todoListContent: String) = """
+            |# 📝${LocalDateTime.now().toStringDateTime()} / TODOLIST📝
+            |$todoListContent
+        """.trimMargin()
+
+    fun createReadMe(
+        token: String,
+        owner: String,
+        repo: String,
+        content: String
+    ) {
+        val branches = gitHubReadMeClient.getBranches(token, owner, repo)
+        val defaultBranch = branches.firstOrNull { it.name == "main" || it.name == "master" }
+            ?: throw IllegalStateException("No b ranches found")
+
+        val newReadme = gitHubReadMeClient.getFile(token, owner, repo, GitHubService.PATH)
+
+        val fileCommitRequest =
+            fileCommitRequestInit(owner, content, defaultBranch.name, newReadme.sha)
+
+        gitHubReadMeClient.createOrUpdateFile(
+            token = token,
+            owner = owner,
+            repo = repo,
+            path = "README.md",
+            body = fileCommitRequest
+        )
+    }
+
+}
