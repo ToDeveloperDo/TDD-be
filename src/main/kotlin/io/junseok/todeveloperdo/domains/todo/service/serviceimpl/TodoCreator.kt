9d0b8631692d8e8fc22066596c4093fcff38136d
JunRock <wnstjr120422@naver.com>
2024-07-15 17:48:11 +0900

    feat : Todo 구현체

--- src/main/kotlin/io/junseok/todeveloperdo/domains/todo/service/serviceimpl/TodoCreator.kt
+++ src/main/kotlin/io/junseok/todeveloperdo/domains/todo/service/serviceimpl/TodoCreator.kt
@@ -3,20 +3,20 @@ package io.junseok.todeveloperdo.domains.todo.service.serviceimpl
 import io.junseok.todeveloperdo.domains.member.persistence.entity.Member
 import io.junseok.todeveloperdo.domains.todo.persistence.entity.MemberTodoList
 import io.junseok.todeveloperdo.domains.todo.persistence.entity.TodoStatus
-import io.junseok.todeveloperdo.presentation.membertodolist.dto.request.TodoCreateRequest
+import io.junseok.todeveloperdo.presentation.membertodolist.dto.request.TodoRequest
 import org.springframework.stereotype.Component
 
 @Component
 class TodoCreator {
     fun generatorTodo(
-        todoCreateRequest: TodoCreateRequest,
+        todoRequest: TodoRequest,
         member: Member,
         issueNumber: Int? = null
     ) = MemberTodoList(
-        content = todoCreateRequest.content,
-        memo = todoCreateRequest.memo,
-        tag = todoCreateRequest.tag,
-        deadline = todoCreateRequest.deadline,
+        content = todoRequest.content,
+        memo = todoRequest.memo,
+        tag = todoRequest.tag,
+        deadline = todoRequest.deadline,
         todoStatus = TodoStatus.PROCEED,
         issueNumber = issueNumber,
         member = member
--- src/main/kotlin/io/junseok/todeveloperdo/domains/todo/service/serviceimpl/TodoUpdater.kt
+++ src/main/kotlin/io/junseok/todeveloperdo/domains/todo/service/serviceimpl/TodoUpdater.kt
@@ -1,5 +1,24 @@
 package io.junseok.todeveloperdo.domains.todo.service.serviceimpl
 
 import io.junseok.todeveloperdo.domains.todo.persistence.entity.MemberTodoList
+import io.junseok.todeveloperdo.presentation.membertodolist.dto.request.TodoRequest
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
-fun MemberTodoList.doneTodoList() = this.updateTodoStatus()
+@Component
+class TodoUpdater() {
+    @Transactional
+    fun update(memberTodoList: MemberTodoList, todoRequest: TodoRequest) {
+        memberTodoList.updateTodoList(
+            todoRequest.content,
+            todoRequest.memo!!,
+            todoRequest.tag,
+            todoRequest.deadline
+        )
+    }
+
+    @Transactional
+    fun doneTodoList(memberTodoList: MemberTodoList) {
+        memberTodoList.updateTodoStatus()
+    }
+}
