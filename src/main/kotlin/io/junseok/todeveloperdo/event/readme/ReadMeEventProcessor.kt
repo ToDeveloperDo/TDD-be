6d1a2f0f43435ed403126e3caeae2f20e15d285c
JunRock <wnstjr120422@naver.com>
2024-07-25 10:49:55 +0900

    refactor : unused import delete

--- src/main/kotlin/io/junseok/todeveloperdo/event/readme/ReadMeEventProcessor.kt
+++ src/main/kotlin/io/junseok/todeveloperdo/event/readme/ReadMeEventProcessor.kt
@@ -3,7 +3,6 @@ package io.junseok.todeveloperdo.event.readme
 import io.junseok.todeveloperdo.domains.member.persistence.entity.Member
 import io.junseok.todeveloperdo.event.readme.dto.ReadMeEventRequest
 import org.springframework.context.ApplicationEventPublisher
-import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 
 @Component
