4be2ef8b2ab275fcf2b0e3e70fc8f00eab170573
JunRock <wnstjr120422@naver.com>
2024-07-29 22:01:51 +0900

    feat : config 파일 설정

---
+++ src/main/kotlin/io/junseok/todeveloperdo/oauth/apple/AppleConfig.kt
@@ -0,0 +1,22 @@
+package io.junseok.todeveloperdo.oauth.apple
+
+import feign.codec.Decoder
+import feign.codec.Encoder
+import feign.form.FormEncoder
+import feign.jackson.JacksonDecoder
+import feign.jackson.JacksonEncoder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class AppleConfig {
+    @Bean
+    fun feignFormEncoder(): Encoder {
+        return FormEncoder(JacksonEncoder())
+    }
+
+    @Bean
+    fun feignDecoder(): Decoder {
+        return JacksonDecoder()
+    }
+}
--- src/main/kotlin/io/junseok/todeveloperdo/oauth/apple/client/AppleClient.kt
+++ src/main/kotlin/io/junseok/todeveloperdo/oauth/apple/client/AppleClient.kt
@@ -1,19 +1,16 @@
 package io.junseok.todeveloperdo.oauth.apple.client
 
 import feign.Headers
+import io.junseok.todeveloperdo.oauth.apple.AppleConfig
 import io.junseok.todeveloperdo.oauth.apple.dto.response.ApplePublicKeys
 import io.junseok.todeveloperdo.oauth.apple.dto.response.AppleTokenResponse
-import io.junseok.todeveloperdo.oauth.git.config.FeignConfig
-import io.junseok.todeveloperdo.oauth.git.config.GitHubClientConfig
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.http.MediaType
-import org.springframework.util.MultiValueMap
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 
-@FeignClient(name = "appleClient", url = "https://appleid.apple.com",configuration = [GitHubClientConfig::class])
+@FeignClient(name = "appleClient", url = "https://appleid.apple.com",configuration = [AppleConfig::class])
 interface AppleClient {
     @PostMapping(value = ["/auth/token"], consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
     @Headers("Content-Type: application/x-www-form-urlencoded")
