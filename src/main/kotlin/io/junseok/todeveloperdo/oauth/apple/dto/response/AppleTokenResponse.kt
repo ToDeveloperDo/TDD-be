69ea837dc4dcfa9985dd9ec73b189d6b3ab30590
JunRock <wnstjr120422@naver.com>
2024-07-29 22:13:03 +0900

    fix : config 파일 설정

--- src/main/kotlin/io/junseok/todeveloperdo/oauth/apple/dto/response/AppleTokenResponse.kt
+++ src/main/kotlin/io/junseok/todeveloperdo/oauth/apple/dto/response/AppleTokenResponse.kt
@@ -1,9 +1,12 @@
 package io.junseok.todeveloperdo.oauth.apple.dto.response
 
-data class AppleTokenResponse(
-    val access_token: String,
-    val token_type: String,
-    val expires_in: Long,
-    val refresh_token: String,
-    val id_token: String
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class AppleTokenResponse @JsonCreator constructor(
+    @JsonProperty("access_token") val accessToken: String,
+    @JsonProperty("token_type") val tokenType: String,
+    @JsonProperty("expires_in") val expiresIn: Long,
+    @JsonProperty("refresh_token") val refreshToken: String,
+    @JsonProperty("id_token") val idToken: String
 )
