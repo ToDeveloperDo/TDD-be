0ce94ac4fe57ca21caa711178d7ea77cef3c21d4
JunRock <wnstjr120422@naver.com>
2024-07-30 22:00:12 +0900

    refactor : unused class delete

--- src/main/kotlin/io/junseok/todeveloperdo/oauth/apple/AppleMemberRepository.kt
+++
@@ -1,9 +0,0 @@
-/*
-package io.junseok.todeveloperdo.oauth.apple
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AppleMemberRepository : JpaRepository<AppleMember,Long> {
-    fun findByAppleId(appleId: String): AppleMember?
-
-}*/
