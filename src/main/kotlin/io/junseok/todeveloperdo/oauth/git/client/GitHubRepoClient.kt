e18b570d81be9c15b7405c2b0b7625bf3e7f7668
JunRock <wnstjr120422@naver.com>
2024-08-06 22:27:57 +0900

    feat : 깃허브 서버에서 repo 존재 여부 확인

--- src/main/kotlin/io/junseok/todeveloperdo/oauth/git/client/GitHubRepoClient.kt
+++ src/main/kotlin/io/junseok/todeveloperdo/oauth/git/client/GitHubRepoClient.kt
@@ -3,6 +3,9 @@ package io.junseok.todeveloperdo.oauth.git.client
 import io.junseok.todeveloperdo.oauth.git.config.GitHubRepoConfig
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
@@ -18,4 +21,11 @@ interface GitHubRepoClient {
         @RequestHeader("Authorization") token: String,
         @RequestBody body: Map<String, Any>
     ): Map<String, Any>
+
+    @GetMapping("/repos/{owner}/{repo}")
+    fun isExistRepo(
+        @PathVariable("owner") owner: String,
+        @PathVariable("repo") repo: String,
+        @PathVariable("Authorization") token: String
+    ): ResponseEntity<Map<String,Any>>
 }
--- src/main/kotlin/io/junseok/todeveloperdo/oauth/git/service/reposerviceimpl/GitHubRepoValidator.kt
+++ src/main/kotlin/io/junseok/todeveloperdo/oauth/git/service/reposerviceimpl/GitHubRepoValidator.kt
@@ -1,16 +1,32 @@
 package io.junseok.todeveloperdo.oauth.git.service.reposerviceimpl
 
 import io.junseok.todeveloperdo.domains.member.service.serviceimpl.MemberReader
+import io.junseok.todeveloperdo.domains.member.service.serviceimpl.MemberValidator
 import io.junseok.todeveloperdo.exception.ErrorCode
 import io.junseok.todeveloperdo.exception.ToDeveloperDoException
+import io.junseok.todeveloperdo.oauth.git.client.GitHubRepoClient
+import io.junseok.todeveloperdo.oauth.git.util.toGeneratorBearerToken
 import org.springframework.stereotype.Component
 
 @Component
 class GitHubRepoValidator(
     private val memberReader: MemberReader,
+    private val memberValidator: MemberValidator,
+    private val gitHubRepoClient: GitHubRepoClient
 ) {
-    fun isExistRepo(appleId: String){
+    fun isExistRepo(appleId: String) {
         val member = memberReader.getMember(appleId)
-        member.gitHubRepo?: throw ToDeveloperDoException{ErrorCode.NOT_EXIST_REPO}
+        memberValidator.isExistRepo(member) // DB에서 repo 유무 확인
+
+        try {
+            gitHubRepoClient.isExistRepo(
+                member.gitHubUsername!!,
+                member.gitHubRepo!!,
+                member.gitHubToken!!.toGeneratorBearerToken()
+            ).statusCode.is2xxSuccessful
+        } catch (e: Exception) {
+            throw ToDeveloperDoException { ErrorCode.NOT_EXIST_REPO }
+        }
+
     }
 }
