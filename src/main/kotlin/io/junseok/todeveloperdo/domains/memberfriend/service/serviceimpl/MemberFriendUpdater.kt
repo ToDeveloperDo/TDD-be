ab7a888948f95bdc93be5442ad14ca0b92e9dbb5
JunRock <wnstjr120422@naver.com>
2024-08-11 00:27:56 +0900

    feat : MemberFriend 구현로직 추가

--- src/main/kotlin/io/junseok/todeveloperdo/domains/memberfriend/service/serviceimpl/MemberFriendUpdater.kt
+++ src/main/kotlin/io/junseok/todeveloperdo/domains/memberfriend/service/serviceimpl/MemberFriendUpdater.kt
@@ -2,8 +2,10 @@ package io.junseok.todeveloperdo.domains.memberfriend.service.serviceimpl
 
 import io.junseok.todeveloperdo.domains.memberfriend.persistence.entity.MemberFriend
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 @Component
 class MemberFriendUpdater {
+    @Transactional
     fun updateStatus(memberFriend: MemberFriend) = memberFriend.updateFriendStatus()
 }
