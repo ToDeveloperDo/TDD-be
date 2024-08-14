d7114ef2202006b5c517731061334f628119f31d
JunRock <wnstjr120422@naver.com>
2024-08-14 16:04:13 +0900

    refactor : unused method delete

--- src/main/kotlin/io/junseok/todeveloperdo/domains/member/service/serviceimpl/MemberCreator.kt
+++ src/main/kotlin/io/junseok/todeveloperdo/domains/member/service/serviceimpl/MemberCreator.kt
@@ -1,15 +1,20 @@
 package io.junseok.todeveloperdo.domains.member.service.serviceimpl
 
+import io.junseok.todeveloperdo.domains.member.persistence.entity.Authority
 import io.junseok.todeveloperdo.domains.member.persistence.entity.Member
 import io.junseok.todeveloperdo.oauth.git.dto.response.GitUserResponse
 import org.springframework.stereotype.Component
 
 @Component
 class MemberCreator() {
-    fun generatorMember(gitUserResponse: GitUserResponse,accessToken:String) = Member(
-        gitHubUsername = gitUserResponse.username,
-        avatarUrl = gitUserResponse.avatarUrl,
-        gitHubUrl = gitUserResponse.gitUrl,
-        gitHubToken = accessToken,
+    fun generatorAppleMember(
+        appleId: String,
+        email: String,
+        refreshToken: String
+    ) = Member(
+        appleId = appleId,
+        appleEmail = email,
+        appleRefreshToken = refreshToken,
+        authority = Authority.ROLE_USER
     )
 }
