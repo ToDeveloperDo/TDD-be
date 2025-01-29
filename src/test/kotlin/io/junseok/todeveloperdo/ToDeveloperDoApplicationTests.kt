40613a57b93cf4ac942ecd610da2a8ce40665933
JunRock <wnstjr120422@naver.com>
2025-01-29 19:30:01 +0900

    fix : Spring Context에러 수정

--- src/test/kotlin/io/junseok/todeveloperdo/ToDeveloperDoApplicationTests.kt
+++ src/test/kotlin/io/junseok/todeveloperdo/ToDeveloperDoApplicationTests.kt
@@ -3,7 +3,10 @@ package io.junseok.todeveloperdo
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest(classes = [ToDeveloperDoApplication::class])
+@SpringBootTest(
+    classes = [ToDeveloperDoApplication::class],
+    webEnvironment = SpringBootTest.WebEnvironment.NONE
+)
 class ToDeveloperDoApplicationTests {
 
     @Test
