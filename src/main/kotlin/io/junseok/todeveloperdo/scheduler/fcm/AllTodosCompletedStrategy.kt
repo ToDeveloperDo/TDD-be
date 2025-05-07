fec4ff32efbaa3ab2ed8f60e1b134c95f03a28e1
JunRock <wnstjr120422@naver.com>
2025-05-07 21:12:53 +0900

    refactor : TimeProvider implement

--- src/main/kotlin/io/junseok/todeveloperdo/scheduler/fcm/AllTodosCompletedStrategy.kt
+++ src/main/kotlin/io/junseok/todeveloperdo/scheduler/fcm/AllTodosCompletedStrategy.kt
@@ -4,18 +4,19 @@ import io.junseok.todeveloperdo.domains.todo.persistence.entity.TodoStatus
 import io.junseok.todeveloperdo.domains.todo.service.serviceimpl.TodoReader
 import io.junseok.todeveloperdo.global.fcm.dto.request.FcmRequest
 import io.junseok.todeveloperdo.global.fcm.dto.request.toFcmRequest
+import io.junseok.todeveloperdo.scheduler.TimeProvider
 import io.junseok.todeveloperdo.scheduler.fcm.NotificationType.ALL_TODOS_COMPLETED
 import org.springframework.stereotype.Component
 import java.time.LocalDate
 
 @Component
 class AllTodosCompletedStrategy(
-    private val todoReader: TodoReader
-
+    private val todoReader: TodoReader,
+    private val timeProvider: TimeProvider
 ) : NotificationStrategy{
     override fun getFcmRequests(): List<FcmRequest> =
         todoReader.findTodoListByTodoStatus(
-            LocalDate.now(),
+            timeProvider.now(),
             TodoStatus.DONE
         ).map { it.toFcmRequest() }.distinct()
             .filter { it.clientToken.isNotBlank() }
