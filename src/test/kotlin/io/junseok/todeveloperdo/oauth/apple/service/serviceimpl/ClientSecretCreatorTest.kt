b67d182c4d616ebcb877ea44def920a2d432dfc8
JunRock <wnstjr120422@naver.com>
2025-05-15 20:36:11 +0900

    test : ClientSecretCreatorTest Testcode Create

---
+++ src/test/kotlin/io/junseok/todeveloperdo/oauth/apple/service/serviceimpl/ClientSecretCreatorTest.kt
@@ -0,0 +1,24 @@
+package io.junseok.todeveloperdo.oauth.apple.service.serviceimpl
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+
+class ClientSecretCreatorTest : FunSpec({
+    test("정상적인 JWT를 생성한다") {
+        val privateKey = System.getenv("APPLE_PRIVATE_KEY")
+            ?.replace("\\n", "\n")
+            ?: throw IllegalStateException("APPLE_PRIVATE_KEY not set")
+
+        val creator = ClientSecretCreator(
+            clientId = "com.example.app",
+            teamId = "TEAMID123",
+            keyId = "KEYID456",
+            privateKey = privateKey
+        )
+
+        val jwt = creator.createClientSecret()
+        jwt shouldNotBe null
+        jwt.split(".").size shouldBe 3
+    }
+})
